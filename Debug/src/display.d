3b146d3196b1a054904484a43b79c8253f72c1bf
Samuli Helttunen <gwchilos@hotmail.com>
2016-10-12 12:03:13 +0300

    Exercise finished

---
+++ Debug/src/display.d
@@ -0,0 +1,13 @@
+src/display.o src/display.d: ../src/display.cpp \
+ /mnt/hgfs/P/LPCXpresso/workspace/SQAT_embedded_TM/inc/i2c.h \
+ /mnt/hgfs/P/LPCXpresso/workspace/SQAT_embedded_TM/inc/hardware.h \
+ /mnt/hgfs/P/LPCXpresso/workspace/SQAT_embedded_TM/inc/ht16k33.h \
+ /mnt/hgfs/P/LPCXpresso/workspace/SQAT_embedded_TM/inc/display.h
+
+/mnt/hgfs/P/LPCXpresso/workspace/SQAT_embedded_TM/inc/i2c.h:
+
+/mnt/hgfs/P/LPCXpresso/workspace/SQAT_embedded_TM/inc/hardware.h:
+
+/mnt/hgfs/P/LPCXpresso/workspace/SQAT_embedded_TM/inc/ht16k33.h:
+
+/mnt/hgfs/P/LPCXpresso/workspace/SQAT_embedded_TM/inc/display.h:
