0e94ff261cbbb7e5ca1931cbc971557c8297df7f
Ahmet Ozan Tatlisu <ahmetozan789@gmail.com>
2016-10-12 12:27:06 +0300

    Ahmet Ozan Tatlisu not done :(

---
+++ Debug/src/TemperatureMonitoring.d
@@ -0,0 +1,131 @@
+src/TemperatureMonitoring.o src/TemperatureMonitoring.d: \
+ ../src/TemperatureMonitoring.cpp \
+ /mnt/hgfs/P/LPCXpresso/workspace/lpc_chip_8xx/inc/chip.h \
+ /mnt/hgfs/P/LPCXpresso/workspace/lpc_chip_8xx/inc/lpc_types.h \
+ /mnt/hgfs/P/LPCXpresso/workspace/lpc_chip_8xx/inc/sys_config.h \
+ /mnt/hgfs/P/LPCXpresso/workspace/lpc_chip_8xx/inc/cmsis.h \
+ /mnt/hgfs/P/LPCXpresso/workspace/lpc_chip_8xx/inc/core_cm0plus.h \
+ /mnt/hgfs/P/LPCXpresso/workspace/lpc_chip_8xx/inc/core_cmInstr.h \
+ /mnt/hgfs/P/LPCXpresso/workspace/lpc_chip_8xx/inc/core_cmFunc.h \
+ /mnt/hgfs/P/LPCXpresso/workspace/lpc_chip_8xx/inc/romapi_8xx.h \
+ /mnt/hgfs/P/LPCXpresso/workspace/lpc_chip_8xx/inc/iap.h \
+ /mnt/hgfs/P/LPCXpresso/workspace/lpc_chip_8xx/inc/error_8xx.h \
+ /mnt/hgfs/P/LPCXpresso/workspace/lpc_chip_8xx/inc/rom_i2c_8xx.h \
+ /mnt/hgfs/P/LPCXpresso/workspace/lpc_chip_8xx/inc/rom_pwr_8xx.h \
+ /mnt/hgfs/P/LPCXpresso/workspace/lpc_chip_8xx/inc/rom_uart_8xx.h \
+ /mnt/hgfs/P/LPCXpresso/workspace/lpc_chip_8xx/inc/syscon_8xx.h \
+ /mnt/hgfs/P/LPCXpresso/workspace/lpc_chip_8xx/inc/clock_8xx.h \
+ /mnt/hgfs/P/LPCXpresso/workspace/lpc_chip_8xx/inc/iocon_8xx.h \
+ /mnt/hgfs/P/LPCXpresso/workspace/lpc_chip_8xx/inc/swm_8xx.h \
+ /mnt/hgfs/P/LPCXpresso/workspace/lpc_chip_8xx/inc/fmc_8xx.h \
+ /mnt/hgfs/P/LPCXpresso/workspace/lpc_chip_8xx/inc/pinint_8xx.h \
+ /mnt/hgfs/P/LPCXpresso/workspace/lpc_chip_8xx/inc/pmu_8xx.h \
+ /mnt/hgfs/P/LPCXpresso/workspace/lpc_chip_8xx/inc/acmp_8xx.h \
+ /mnt/hgfs/P/LPCXpresso/workspace/lpc_chip_8xx/inc/crc_8xx.h \
+ /mnt/hgfs/P/LPCXpresso/workspace/lpc_chip_8xx/inc/gpio_8xx.h \
+ /mnt/hgfs/P/LPCXpresso/workspace/lpc_chip_8xx/inc/mrt_8xx.h \
+ /mnt/hgfs/P/LPCXpresso/workspace/lpc_chip_8xx/inc/uart_8xx.h \
+ /mnt/hgfs/P/LPCXpresso/workspace/lpc_chip_8xx/inc/ring_buffer.h \
+ /mnt/hgfs/P/LPCXpresso/workspace/lpc_chip_8xx/inc/wkt_8xx.h \
+ /mnt/hgfs/P/LPCXpresso/workspace/lpc_chip_8xx/inc/wwdt_8xx.h \
+ /mnt/hgfs/P/LPCXpresso/workspace/lpc_chip_8xx/inc/sct_8xx.h \
+ /mnt/hgfs/P/LPCXpresso/workspace/lpc_chip_8xx/inc/sct_pwm_8xx.h \
+ /mnt/hgfs/P/LPCXpresso/workspace/lpc_chip_8xx/inc/spi_8xx.h \
+ /mnt/hgfs/P/LPCXpresso/workspace/lpc_chip_8xx/inc/spi_common_8xx.h \
+ /mnt/hgfs/P/LPCXpresso/workspace/lpc_chip_8xx/inc/i2cm_8xx.h \
+ /mnt/hgfs/P/LPCXpresso/workspace/lpc_chip_8xx/inc/i2c_common_8xx.h \
+ /mnt/hgfs/P/LPCXpresso/workspace/lpc_chip_8xx/inc/i2cs_8xx.h \
+ /mnt/hgfs/P/LPCXpresso/workspace/lpc_chip_8xx/inc/spim_8xx.h \
+ /mnt/hgfs/P/LPCXpresso/workspace/lpc_chip_8xx/inc/spis_8xx.h \
+ /mnt/hgfs/P/LPCXpresso/workspace/lpc_chip_8xx/inc/irc_8xx.h \
+ /mnt/hgfs/P/LPCXpresso/workspace/SQAT_embedded_TM/inc/swm.h \
+ /mnt/hgfs/P/LPCXpresso/workspace/SQAT_embedded_TM/inc/delay.h \
+ /mnt/hgfs/P/LPCXpresso/workspace/SQAT_embedded_TM/inc/i2c.h \
+ /mnt/hgfs/P/LPCXpresso/workspace/SQAT_embedded_TM/inc/display.h \
+ /mnt/hgfs/P/LPCXpresso/workspace/SQAT_embedded_TM/inc/tm.h
+
+/mnt/hgfs/P/LPCXpresso/workspace/lpc_chip_8xx/inc/chip.h:
+
+/mnt/hgfs/P/LPCXpresso/workspace/lpc_chip_8xx/inc/lpc_types.h:
+
+/mnt/hgfs/P/LPCXpresso/workspace/lpc_chip_8xx/inc/sys_config.h:
+
+/mnt/hgfs/P/LPCXpresso/workspace/lpc_chip_8xx/inc/cmsis.h:
+
+/mnt/hgfs/P/LPCXpresso/workspace/lpc_chip_8xx/inc/core_cm0plus.h:
+
+/mnt/hgfs/P/LPCXpresso/workspace/lpc_chip_8xx/inc/core_cmInstr.h:
+
+/mnt/hgfs/P/LPCXpresso/workspace/lpc_chip_8xx/inc/core_cmFunc.h:
+
+/mnt/hgfs/P/LPCXpresso/workspace/lpc_chip_8xx/inc/romapi_8xx.h:
+
+/mnt/hgfs/P/LPCXpresso/workspace/lpc_chip_8xx/inc/iap.h:
+
+/mnt/hgfs/P/LPCXpresso/workspace/lpc_chip_8xx/inc/error_8xx.h:
+
+/mnt/hgfs/P/LPCXpresso/workspace/lpc_chip_8xx/inc/rom_i2c_8xx.h:
+
+/mnt/hgfs/P/LPCXpresso/workspace/lpc_chip_8xx/inc/rom_pwr_8xx.h:
+
+/mnt/hgfs/P/LPCXpresso/workspace/lpc_chip_8xx/inc/rom_uart_8xx.h:
+
+/mnt/hgfs/P/LPCXpresso/workspace/lpc_chip_8xx/inc/syscon_8xx.h:
+
+/mnt/hgfs/P/LPCXpresso/workspace/lpc_chip_8xx/inc/clock_8xx.h:
+
+/mnt/hgfs/P/LPCXpresso/workspace/lpc_chip_8xx/inc/iocon_8xx.h:
+
+/mnt/hgfs/P/LPCXpresso/workspace/lpc_chip_8xx/inc/swm_8xx.h:
+
+/mnt/hgfs/P/LPCXpresso/workspace/lpc_chip_8xx/inc/fmc_8xx.h:
+
+/mnt/hgfs/P/LPCXpresso/workspace/lpc_chip_8xx/inc/pinint_8xx.h:
+
+/mnt/hgfs/P/LPCXpresso/workspace/lpc_chip_8xx/inc/pmu_8xx.h:
+
+/mnt/hgfs/P/LPCXpresso/workspace/lpc_chip_8xx/inc/acmp_8xx.h:
+
+/mnt/hgfs/P/LPCXpresso/workspace/lpc_chip_8xx/inc/crc_8xx.h:
+
+/mnt/hgfs/P/LPCXpresso/workspace/lpc_chip_8xx/inc/gpio_8xx.h:
+
+/mnt/hgfs/P/LPCXpresso/workspace/lpc_chip_8xx/inc/mrt_8xx.h:
+
+/mnt/hgfs/P/LPCXpresso/workspace/lpc_chip_8xx/inc/uart_8xx.h:
+
+/mnt/hgfs/P/LPCXpresso/workspace/lpc_chip_8xx/inc/ring_buffer.h:
+
+/mnt/hgfs/P/LPCXpresso/workspace/lpc_chip_8xx/inc/wkt_8xx.h:
+
+/mnt/hgfs/P/LPCXpresso/workspace/lpc_chip_8xx/inc/wwdt_8xx.h:
+
+/mnt/hgfs/P/LPCXpresso/workspace/lpc_chip_8xx/inc/sct_8xx.h:
+
+/mnt/hgfs/P/LPCXpresso/workspace/lpc_chip_8xx/inc/sct_pwm_8xx.h:
+
+/mnt/hgfs/P/LPCXpresso/workspace/lpc_chip_8xx/inc/spi_8xx.h:
+
+/mnt/hgfs/P/LPCXpresso/workspace/lpc_chip_8xx/inc/spi_common_8xx.h:
+
+/mnt/hgfs/P/LPCXpresso/workspace/lpc_chip_8xx/inc/i2cm_8xx.h:
+
+/mnt/hgfs/P/LPCXpresso/workspace/lpc_chip_8xx/inc/i2c_common_8xx.h:
+
+/mnt/hgfs/P/LPCXpresso/workspace/lpc_chip_8xx/inc/i2cs_8xx.h:
+
+/mnt/hgfs/P/LPCXpresso/workspace/lpc_chip_8xx/inc/spim_8xx.h:
+
+/mnt/hgfs/P/LPCXpresso/workspace/lpc_chip_8xx/inc/spis_8xx.h:
+
+/mnt/hgfs/P/LPCXpresso/workspace/lpc_chip_8xx/inc/irc_8xx.h:
+
+/mnt/hgfs/P/LPCXpresso/workspace/SQAT_embedded_TM/inc/swm.h:
+
+/mnt/hgfs/P/LPCXpresso/workspace/SQAT_embedded_TM/inc/delay.h:
+
+/mnt/hgfs/P/LPCXpresso/workspace/SQAT_embedded_TM/inc/i2c.h:
+
+/mnt/hgfs/P/LPCXpresso/workspace/SQAT_embedded_TM/inc/display.h:
+
+/mnt/hgfs/P/LPCXpresso/workspace/SQAT_embedded_TM/inc/tm.h:
